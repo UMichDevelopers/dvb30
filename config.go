01222f8467f3682b8293ab800dd4cb4bf7401be1
Runxi Yu <runxiyu@umich.edu>
2026-03-20 04:59:42 +0000

    basic config

---
+++ config.go
@@ -0,0 +1,27 @@
+package main
+
+type Config struct {
+	Discord struct {
+		Token   string `scfgs:"token"`
+		GuildID uint64 `scfgs:"guild_id"`
+	} `scfgs:"discord"`
+
+	Google struct {
+		ClientID     string `scfgs:"client_id"`
+		ClientSecret string `scfgs:"client_secret"`
+		RedirectURL  string `scfgs:"redirect_url"`
+	} `scfgs:"google"`
+
+	HTTP struct {
+		Net     string `scfgs:"net"`
+		Addr    string `scfgs:"addr"`
+		BaseURL string `scfgs:"base_url"`
+	} `scfgs:"http"`
+
+	Roles struct {
+		Verified uint64 `scfgs:"verified"`
+	} `scfgs:"roles"`
+
+	CookieSecret       string `scfgs:"cookie_secret"`
+	TokenExpirySeconds int    `scfgs:"token_expiry_seconds"`
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/UMichDevelopers/dvb30
+
+go 1.26.1
+
+require codeberg.org/lindenii/go-scfgs v0.0.0-20260114120835-34ea387cf308 // indirect
